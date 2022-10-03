010e0433457036b8960dbbdd6cbf19fc6122325d
Dmitrii Belik <belikdmi@fit.cvut.cz>
2022-10-03 06:10:59 +0200

    Minor changes

--- web/public/assets/js/api.js
+++ web/public/assets/js/api.js
@@ -62,7 +62,7 @@ function ajax_t3(){$.ajax({
     contentType: 'application/json',
     processData: false,
     success: function( data, textStatus, jQxhr ){
-        $('#resp').html( JSON.stringify( data ) );
+        $('#punchline-1').html( JSON.stringify( data ) );
     },
     error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
@@ -85,7 +85,7 @@ function ajax_t4(){$.ajax({
     contentType: 'application/json',
     processData: false,
     success: function( data, textStatus, jQxhr ){
-        $('#resp').html( JSON.stringify( data ) );
+        $('#punchline-1').html( JSON.stringify( data ) );
     },
     error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
@@ -109,7 +109,7 @@ function ajax_t5(){$.ajax({
     contentType: 'application/json',
     processData: false,
     success: function( data, textStatus, jQxhr ){
-        $('#resp').html( JSON.stringify( data ) );
+        $('#punchline-3').html( JSON.stringify( data ) );
     },
     error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
@@ -132,7 +132,105 @@ function ajax_t6(){$.ajax({
     contentType: 'application/json',
     processData: false,
     success: function( data, textStatus, jQxhr ){
+        $('#punchline-3').html( JSON.stringify( data ) );
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
+    }
+});}
+
+
+
+
+
+document.addEventListener("click", function (event) {
+  // Checking if the button was clicked
+  if (!event.target.matches("#compinfou")) return;
+
+
+    ajax_t11();
+});
+
+function ajax_t11(){$.ajax({
+    crossdomain: true,
+    url: 'https://loyaton.herokuapp.com/api/user?companyID=' + $('#copmidu').val(),
+    dataType: 'json',
+    type: 'get',
+    contentType: 'application/json',
+    processData: false,
+    success: function( data, textStatus, jQxhr ){
         $('#resp').html( JSON.stringify( data ) );
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
+    }
+});}
+
+document.addEventListener("click", function (event) {
+  // Checking if the button was clicked
+  if (!event.target.matches("#regcompu")) return;
+
+
+    ajax_t12();
+});
+
+function ajax_t12(){$.ajax({
+    crossdomain: true,
+    url: 'https://loyaton.herokuapp.com/api/user',
+    dataType: 'json',
+    type: 'post',
+    data: JSON.stringify( { "email": $('#emailu').val(), "name": $('#nameu').val(), "password": $('#passu').val() } ),
+    contentType: 'application/json',
+    processData: false,
+    success: function( data, textStatus, jQxhr ){
+        $('#resp').html( JSON.stringify( data ) );
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
+    }
+});}
+
+document.addEventListener("click", function (event) {
+  // Checking if the button was clicked
+  if (!event.target.matches("#createrewardu")) return;
+
+
+    ajax_t13();
+});
+
+function ajax_t13(){$.ajax({
+    crossdomain: true,
+    url: 'https://loyaton.herokuapp.com/api/reward',
+    dataType: 'json',
+    type: 'post',
+    data: JSON.stringify( { "userID": $('#rewardidu').val(), "rewardID": $('#rewardnameu').val() } ),
+    contentType: 'application/json',
+    processData: false,
+    success: function( data, textStatus, jQxhr ){
+        $('#punchline-11').html( JSON.stringify( data ) );
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
+    }
+});}
+
+document.addEventListener("click", function (event) {
+  // Checking if the button was clicked
+  if (!event.target.matches("#regcomp-2")) return;
+
+
+    ajax_t14();
+});
+
+function ajax_t14(){$.ajax({
+    crossdomain: true,
+    url: 'https://loyaton.herokuapp.com/api/reward?companyID=' + $('#compidrewu').val(),
+    dataType: 'json',
+    type: 'get',
+    contentType: 'application/json',
+    processData: false,
+    success: function( data, textStatus, jQxhr ){
+        $('#punchline-11').html( JSON.stringify( data ) );
     },
     error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
